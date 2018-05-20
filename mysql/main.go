7250e883f5265bb0cab8e704f7ad040b6aa84d00
xujialong <xjlong@outlook.com>
2018-05-20 22:31:18 +0800

    proto和mysql用法

---
+++ mysql/main.go
@@ -0,0 +1,90 @@
+package main
+
+import(
+	_ "github.com/go-sql-driver/mysql"
+	"database/sql"
+	"fmt"
+	"github.com/golang/protobuf/proto"
+	"github.com/0990/golearn/mysql/pb"
+)
+
+func getPb()[]byte{
+	contact := test.Contact{}
+
+	person1:=test.Person{
+		Id:1,
+		Name:"xu",
+		PhoneType:test.PhoneType_HOME,
+	}
+
+	person2 :=test.Person{
+		Id:2,
+		Name:"jia",
+		PhoneType:test.PhoneType_WORK,
+	}
+
+	contact.Person = append(contact.Person, &person1)
+	contact.Person = append(contact.Person,&person2)
+
+	data,_ := proto.Marshal(&contact)
+	return data
+}
+
+func getMapPb()[]byte{
+	complexObj:=test.ComplexObj{}
+	map1:=test.MapValue{
+		Name:"xu",
+		Age:11,
+	}
+	map2:=test.MapValue{
+		Name:"jia",
+		Age:23,
+	}
+	complexObj.Map = make(map[string]*test.MapValue)
+	complexObj.Map["1"] = &map1;
+	complexObj.Map["2"] = &map2;
+
+	data,_ := proto.Marshal(&complexObj)
+
+	return data
+}
+
+
+func main(){
+	db,err:=sql.Open("mysql","root:123456@tcp(127.0.0.1:3306)/test?charset=utf8")
+	if err!=nil{
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err!=nil{
+		panic(err.Error())
+	}
+
+	//stmt,err:=db.Prepare(`replace into pb(id,data)values(?,?)`)
+	//if err!=nil{
+	//	log.Panic(err)
+	//}
+	//_,err =stmt.Exec(1,getMapPb())
+	//if err!=nil{
+	//	log.Panic(err)
+	//}
+
+	//db.Exec("replace into pb(id,data)values '%s'",)
+	rows,err:=db.Query("select id,data from pb where id = 1")
+	defer rows.Close()
+
+	for rows.Next(){
+		var id int
+		var data []byte
+		err = rows.Scan(&id,&data)
+		comlexObj:=test.ComplexObj{}
+		proto.Unmarshal(data,&comlexObj)
+		fmt.Println(comlexObj)
+	}
+	err = rows.Err()
+	if err!=nil{
+		panic(err.Error())
+	}
+}
--- protobuf/main.go
+++ protobuf/main.go
@@ -1,118 +1,68 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"io"
-	"io/ioutil"
-	"log"
-	"os"
-	"strings"
-
-	protob "github.com/0990/golearn/protobuf/pb"
+	"github.com/0990/golearn/protobuf/pb"
 	"github.com/golang/protobuf/proto"
+	"io/ioutil"
+	"os"
+	"log"
+	"fmt"
 )
 
-func promptForAddress(r io.Reader) (*protob.Person, error) {
-	p := &protob.Person{}
-
-	rd := bufio.NewReader(r)
-	fmt.Print("Enter person ID number:")
-	if _, err := fmt.Fscanf(rd, "%d\n", &p.Id); err != nil {
-		return p, err
-	}
-
-	fmt.Print("Enter name")
-	name, err := rd.ReadString('\n')
-	if err != nil {
-		return p, err
-	}
-
-	p.Name = strings.TrimSpace(name)
-	fmt.Print("Enter email addrss(black for none):")
-	email, err := rd.ReadString('\n')
-	if err != nil {
-		return p, err
-	}
-
-	p.Email = strings.TrimSpace(email)
-
-	for {
-		fmt.Print("Enter a phone number(or leave blank to finish):")
-		phone, err := rd.ReadString('\n')
-		if err != nil {
-			return p, err
-		}
-		phone = strings.TrimSpace(phone)
-		if phone == "" {
-			break
-		}
-
-		pn := &protob.Person_PhoneNumber{
-			Number: phone,
-		}
-
-		fmt.Print("Is this a mobile,hone,or work phone?")
-		ptype, err := rd.ReadString('\n')
-		if err != nil {
-			return p, err
-		}
-		ptype = strings.TrimSpace(ptype)
-
-		switch ptype {
-		case "mobile":
-			pn.Type = protob.Person_MOBILE
-		case "home":
-			pn.Type = protob.Person_HOME
-		case "work":
-			pn.Type = protob.Person_WORK
-		default:
-			fmt.Printf("Unkonw type:%q,USing default.\n", ptype)
-		}
-		p.Phones = append(p.Phones, pn)
-	}
-	return p, nil
+type Student struct{
+	Name string
+	Age int
 }
 
-func main() {
-	if len(os.Args) != 2 {
-		log.Fatal("Usage:%s ADDRESS_BOOK_FILE\n", os.Args[0])
-	}
-	fname := os.Args[1]
+func write(){
+	contact := test.Contact{}
 
-	in, err := ioutil.ReadFile(fname)
-	if err != nil {
-		if os.IsNotExist(err) {
-			fmt.Printf("%s:File not found.Createing new file.\n", fname)
-		} else {
-			log.Fatalln("Error reading file:", err)
-		}
+	person1:=test.Person{
+		Id:1,
+		Name:"xu",
+		PhoneType:test.PhoneType_HOME,
 	}
 
-	book := &protob.AddressBook{}
+	person2 :=test.Person{
+		Id:2,
+		Name:"jia",
+		PhoneType:test.PhoneType_WORK,
+	}
+	//fmt.Println(person1)
 
-	fmt.Println("AddressBook protoMessageName:",proto.MessageName(book))
+	contact.Person = append(contact.Person, &person1)
+	contact.Person = append(contact.Person,&person2)
 
-	if err := proto.Unmarshal(in, book); err != nil {
-		log.Fatalln("Failed to parse address book:", err)
+	data,_ := proto.Marshal(&contact)
+
+	str:=proto.MarshalTextString(&contact)
+
+	conta:=test.Contact{}
+	proto.UnmarshalText(str,&conta)
+
+	fmt.Println(str,conta)
+
+
+	ioutil.WriteFile("./test.txt",data,os.ModePerm)
+}
+
+func read(){
+	data,err := ioutil.ReadFile("./test.txt")
+
+	if err!=nil{
+			log.Fatalln(err)
 	}
 
-	fmt.Println(book)
+	contact:=test.Contact{}
+	proto.Unmarshal(data,&contact)
 
-	addr, err := promptForAddress(os.Stdin)
-
-	if err != nil {
-		log.Fatalln("error with addrsss:", err)
+	for _,v := range contact.Person{
+		fmt.Println(v)
 	}
-	book.People = append(book.People, addr)
+	fmt.Printf("%+v",contact)
+}
 
-	out, err := proto.Marshal(book)
-
-	if err != nil {
-		log.Fatalln("Failed to encode address book:", err)
-	}
-
-	if err := ioutil.WriteFile(fname, out, 0644); err != nil {
-		log.Fatalln("Failed to write address book:", err)
-	}
+func main(){
+	write()
+	//read()
 }
