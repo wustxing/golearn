8ca71bf788df79785ac7d638ff78f77cc0ac9e7c
xujialong <xjlong@outlook.com>
2018-06-25 09:48:19 +0800

    add xujialong#

--- slice/del/main.go
+++ slice/del/main.go
@@ -8,19 +8,22 @@ func main(){
 	for i:=0;i<10;i++{
 		intSlice = append(intSlice,i)
 	}
+	intSlice = append(intSlice,10)
 
 	fmt.Println(intSlice)
+	fmt.Println(intSlice[:])
 	fmt.Println(intSlice[:1])
-	fmt.Println(intSlice[:5])
-	fmt.Println(intSlice[6:])
-
-	for i,v:=range intSlice{
-		if v==5{
-			fmt.Println(i)
-			intSlice = append(intSlice[:i],intSlice[i+1:]...)
-		}
-	}
+	fmt.Println(intSlice[0:])
+	//
+	//for i,v:=range intSlice{
+	//	if v==5{
+	//		fmt.Println(i)
+	//		intSlice = append(intSlice[:i],intSlice[i+1:]...)
+	//	}
+	//}
+	intSlice = intSlice[1:]
+	//intSlice = append(intSlice[:0],intSlice[1:]...)
+	//copy(intSlice,intSlice[1:])
 
 	fmt.Println(intSlice)
-
 }
