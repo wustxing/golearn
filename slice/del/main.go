7979f40147b398be46d4a45b56514ecaacefe65d
xujialong <xjlong@outlook.com>
2018-07-02 17:41:55 +0800

    add xujialong#

--- slice/del/main.go
+++ slice/del/main.go
@@ -5,25 +5,33 @@ import "fmt"
 func main(){
 	intSlice:=make([]int,0)
 
-	for i:=0;i<10;i++{
+	for i:=0;i<2;i++{
 		intSlice = append(intSlice,i)
 	}
-	intSlice = append(intSlice,10)
+	//intSlice = append(intSlice,10)
 
-	fmt.Println(intSlice)
-	fmt.Println(intSlice[:])
-	fmt.Println(intSlice[:1])
-	fmt.Println(intSlice[0:])
+	//fmt.Println(intSlice)
+	//fmt.Println(intSlice[:])
+	//fmt.Println(intSlice[:1])
+	//fmt.Println(intSlice[0:])
 	//
+	//fmt.Println(intSlice)
 	//for i,v:=range intSlice{
-	//	if v==5{
-	//		fmt.Println(i)
+	//	if v==0{
+	//
 	//		intSlice = append(intSlice[:i],intSlice[i+1:]...)
 	//	}
 	//}
-	intSlice = intSlice[1:]
+	//intSlice = intSlice[1:]
 	//intSlice = append(intSlice[:0],intSlice[1:]...)
 	//copy(intSlice,intSlice[1:])
 
-	fmt.Println(intSlice)
+	//fmt.Println(intSlice)
+	addInt(intSlice...)
+}
+
+func addInt(ints ...int){
+	for _,v:=range ints{
+		fmt.Println(v)
+	}
 }
