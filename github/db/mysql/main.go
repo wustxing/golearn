d2cb489d970ecdd19b185e12fd12823f2b5f2516
zuma <xjlong@outlook.com>
2019-04-27 15:43:04 +0800

    reflect

--- github/db/mysql/main.go
+++ github/db/mysql/main.go
@@ -4,12 +4,11 @@ import (
 	"database/sql"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/sirupsen/logrus"
 	"time"
 )
 
 func main() {
-	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/sgs_nuyan?charset=utf8")
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/sgs_nuyan?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err.Error())
 	}
@@ -20,17 +19,13 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
-
-	for i := 0; i < 10000; i++ {
-		v := i
-		go func() {
-			//_, err := db.Exec("Insert into hello(nickname) VALUES (?)", fmt.Sprintf("hi%d", v))
-			err := db.QueryRow("SELECT userid FROM auth WHERE userid=? LIMIT 1;", v).Scan()
-			if err != nil {
-				logrus.WithError(err).Error("insert db error")
-			}
-		}()
+	result, err := db.Exec("INSERT INTO load_balance (name,v,log_time) VALUES (?,?,?)", "game", 1, time.Now())
+	if err != nil {
+		fmt.Println(result, err)
+	}
+	err = db.QueryRow("SELECT userid FROM auth WHERE userid=? LIMIT 1;", 1).Scan()
+	if err != nil {
+		fmt.Println(err)
 	}
 	fmt.Println("over")
-	time.Sleep(time.Minute)
 }
--- main.go
+++ main.go
@@ -1,12 +1,100 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"time"
+	_ "github.com/go-sql-driver/mysql"
+	"reflect"
+	"strings"
 )
 
-func main() {
-	fmt.Println("start")
-	time.Sleep(time.Minute)
-	fmt.Println("end")
+type User struct {
+	Name string `json:"name" field:"name"`
 }
+
+type Data struct {
+	Data interface{}
+}
+
+func main() {
+	u := &User{Name: "xujialong"}
+	data, err := json.Marshal(u)
+	if err != nil {
+		panic(err)
+	}
+
+	datamodel := &Data{
+		Data: (*User)(nil),
+	}
+	typ := reflect.TypeOf(datamodel.Data)
+	elem := typ.Elem()
+	newUser := reflect.New(elem).Interface()
+	err = json.Unmarshal(data, &newUser)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(reflect.ValueOf(newUser))
+	//v:=reflect.ValueOf(newUser).Elem()
+	//for i := 0; i < v.NumField(); i++ {
+	//	val := v.Field(i).Interface()
+	//	fmt.Printf("%v\n",val)
+	//}
+	newUserTyp := reflect.TypeOf(newUser)
+	fmt.Println(newUserTyp, elem)
+
+	//value:=reflect.ValueOf(u)
+	//elem:=typ.Elem()
+	//
+	//typeElem:=reflect.TypeOf(elem)
+	//fmt.Println(elem.NumField(),typ,typ.Kind(),typ.Elem(),value,typeElem)
+	sql, sql1 := model2SqlColumn(newUserTyp.Elem())
+	fmt.Println(sql, sql1)
+
+	//x:=(*User)(nil)
+	//y:=(*Data)(nil)
+	//fmt.Println(x==nil,x==y,y==nil)
+}
+
+func model2SqlColumn(m reflect.Type) (string, string) {
+	//m := reflect.TypeOf(model)
+	//m:=model
+	result := ""
+	valueNumString := ""
+	if m != nil {
+		slice := make([]string, 0, m.NumField())
+		vSlice := make([]string, 0, m.NumField())
+		for i := 0; i < m.NumField(); i++ {
+			tag := m.Field(i).Tag.Get("field")
+			if tag == "-" {
+				continue
+			}
+			slice = append(slice, tag)
+			vSlice = append(vSlice, "?")
+		}
+		result = strings.Join(slice, ", ")
+		valueNumString = strings.Join(vSlice, ",")
+	}
+	return result, valueNumString
+}
+
+//func model2SqlColumnValue(m reflect.Value) (string, string) {
+//	//m := reflect.TypeOf(model)
+//	//m:=model
+//	result := ""
+//	valueNumString := ""
+//	if m != nil {
+//		slice := make([]string, 0, m.NumField())
+//		vSlice := make([]string, 0, m.NumField())
+//		for i := 0; i < m.NumField(); i++ {
+//			tag := m.Field(i).Tag.Get("field")
+//			if tag == "-" {
+//				continue
+//			}
+//			slice = append(slice, tag)
+//			vSlice = append(vSlice, "?")
+//		}
+//		result = strings.Join(slice, ", ")
+//		valueNumString = strings.Join(vSlice, ",")
+//	}
+//	return result, valueNumString
+//}
