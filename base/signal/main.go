09fc5707ab1d09b19a676c922758cd13463e7da6
john <xjlong@outlook.com>
2020-03-31 22:52:40 +0800

    update:signal使用

--- base/signal/main.go
+++ base/signal/main.go
@@ -4,22 +4,51 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"runtime"
+	"syscall"
 	"time"
 )
 
+//SIGKILL SIGSTOP无法被捕捉
+//SIGINT SIGTERM SIGHUP默认处理是关闭程序
+//SIGQUIT 默认处理会打印dump后退出程序
+//SIGSTOP和SIGCONT组合使用可以暂停和恢复程序
+//示例中setupsigusr1Trap 当kill -SIGUSR1 pid时,会打印dump（不会退出）,用于查日志
+//更多 https://books.studygolang.com/The-Golang-Standard-Library-by-Example/chapter16/16.03.html?h=signal
+
 func main() {
+	setupSigusr1Trap()
+	go print()
 	c := make(chan os.Signal, 1)
 	signal.Notify(c)
-	ticker := time.Tick(time.Second * 1)
-	go func() {
-		for {
-			select {
-			case <-ticker:
-				fmt.Println("ticker come")
-			}
-		}
-	}()
-	//time.Sleep(time.Second*10)
 	s := <-c
 	fmt.Println("Got signal:", s)
+}
+
+func print() {
+	t := time.NewTicker(time.Second * 1)
+	defer t.Stop()
+	for {
+		select {
+		case <-t.C:
+			fmt.Println("ticker come,", time.Now())
+		}
+	}
+
+}
+
+func setupSigusr1Trap() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGUSR1)
+	go func() {
+		for range c {
+			DumpStacks()
+		}
+	}()
+}
+
+func DumpStacks() {
+	buf := make([]byte, 16384)
+	buf = buf[:runtime.Stack(buf, true)]
+	fmt.Printf("===BEGIN dump===\n%s\n===END dump", buf)
 }
